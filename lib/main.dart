ec8c86ec8ac02ac55f6ecb59a6825f8aaf56827f
ZabihullahNooriWardak <zabinoori712@gmail.com>
2025-03-26 06:51:03 +0430

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:demo/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(debugShowCheckedModeBanner: false, home: MainScreen()),
+  );
+}
